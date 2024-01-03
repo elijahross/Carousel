f24e419050ceccb4ee92bee50aed1a0ca521f784
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-01-03 10:48:04 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="card">
-        <h1>Spinorama Carousel 🎠</h1>
+        <h1>Spinorama Carousel!</h1>
         <br />
         <br />
         <p>
           Turn your web content into a lively carnival with Spinorama Carousel!
-          🌟 This whimsical carousel component, crafted with CSS and some Java,
+          This whimsical carousel component, crafted with CSS and some Java,
           playfully duplicates the children of a div container and sends them on
           an infinite spinning adventure. Inject fun and excitement into your
           web projects effortlessly!
